3a964f49fc246cf8aad7a7bbe13a11539bd1446a
qpehlivan <70510167+qpehlivan@users.noreply.github.com>
2020-09-01 09:20:01 +0300

    Add files via upload
    Made small adjustments and added comments.

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,11 +2,28 @@ import pandas as pd
 import numpy as np
 
 def execute(input_file, output_file):
+    
     data = pd.read_csv(input_file, sep = ";")
-    data = data.dropna()
+    
     del(data["Name"])
     del(data["Ticket"])
     del(data["Cabin"])
     
-
+    data = data.dropna()
+    
+    #Above, the line with dropna() should NOT go
+    #first. Before that, we must get rid of the
+    #unwanted data.
+    
+    #This prevents deleting usable data.
+    
     data.to_csv(output_file)
+    
+execute("../data/train.csv","../data/train.csv")
+
+execute("../data/val.csv","../data/val.csv")
+#Line 15 and 17 has been added with the file path
+#to execute the function above.
+
+
+#Now, both .csv files are fitted for the purpose.
